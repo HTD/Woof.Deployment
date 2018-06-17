a83b40fe5e9ca557718a02c7cad399bb53bf51c1
Adam Łyskawa <adam@codedog.pl>
2018-06-17 09:57:59 +0200

    ADD: LinkTool and Target classes, version bump to 1.0.3

---
+++ DeploymentEx/LinkTool.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Woof.DeploymentEx {
+
+    /// <summary>
+    /// A tool for finding download links to the latest version of software.
+    /// </summary>
+    public static class LinkTool {
+
+        /// <summary>
+        /// Gets a link from remote page matching the version pattern.
+        /// </summary>
+        /// <param name="uri">Initial page to search.</param>
+        /// <param name="patterns">Patterns to match when following links, use '*' for version dependent string.</param>
+        /// <returns>Link matching the pattern or null if nothing matches.</returns>
+        public static Uri FetchLastVersionLink(Uri uri, params string[] patterns) {
+            foreach (var pattern in patterns) {
+                if (uri == null) return null;
+                uri = GetLink(uri, pattern);
+            }
+            return uri;
+        }
+
+        /// <summary>
+        /// Gets a link from remote page matching the version pattern.
+        /// </summary>
+        /// <param name="uri">URL link to the page to crawl.</param>
+        /// <param name="pattern">Pattern to match, use '*' for version dependent string.</param>
+        /// <returns>Link matching the pattern or null if nothing matches.</returns>
+        /// <remarks>
+        /// Selecting the latest version depends on whether the version strings are sortable in ascending version order.
+        /// </remarks>
+        private static Uri GetLink(Uri uri, string pattern) {
+            String html, link;
+            using (var client = new WebClient()) html = client.DownloadString(uri);
+            if (pattern.Contains('*')) {
+                var regex = new Regex(Regex.Escape(pattern).Replace("\\*", "(.*?)"));
+                link = regex.Matches(html).Cast<Match>().OrderBy(i => i.Groups[1].Value).LastOrDefault()?.Value;
+            }
+            else link = html.IndexOf(pattern, IgnoreCase) > 0 ? pattern : null;
+            if (link == null) return null;
+            return new Uri(link.Contains("://") ? link : $"{uri.Scheme}://{uri.Host}{link}");
+        }
+
+        private const StringComparison IgnoreCase = StringComparison.InvariantCultureIgnoreCase;
+
+    }
+
+}
---
+++ DeploymentEx/Target.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Security.Principal;
+
+namespace Woof.DeploymentEx {
+
+    /// <summary>
+    /// Tools related to system environment paths.
+    /// </summary>
+    public static class Target {
+
+        #region Current process environment
+
+        /// <summary>
+        /// Gets the automatic <see cref="EnvironmentVariableTarget"/> depending on whether current user has administrative privileges.
+        /// </summary>
+        public static EnvironmentVariableTarget Auto {
+            get {
+                using (var identity = WindowsIdentity.GetCurrent())
+                    return new WindowsPrincipal(identity).IsInRole(WindowsBuiltInRole.Administrator)
+                        ? EnvironmentVariableTarget.Machine
+                        : EnvironmentVariableTarget.User;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the "Program Files" folder will be x86 folder.
+        /// (True for programs compiled with "Prefer 32-bit" option set).
+        /// </summary>
+        public static bool IsX86 => Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles).Contains("x86");
+
+        /// <summary>
+        /// Gets program files folder depending on target type (machine / user).
+        /// </summary>
+        /// <param name="target">Environment location.</param>
+        /// <returns>Hopefully writeable directory to store new programs in.</returns>
+        public static string GetProgramFilesDirectory(EnvironmentVariableTarget target) =>
+            target == EnvironmentVariableTarget.Machine
+                ? Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)
+                : Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Programs");
+
+        #endregion
+
+    }
+
+}
